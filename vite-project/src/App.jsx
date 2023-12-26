f3e759c2c94929156ae5dffe9175d5c25b31a3d3
JJ <jonathanjacobwang@gmail.com>
2023-12-26 14:21:45 -0800

    added vite,tailwind

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p className="font-mono text-xl text-blue-500">jj asdfadsf </p>
+    </div>
+      
+  )
+}
+
+export default App
