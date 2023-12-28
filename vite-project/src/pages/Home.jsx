ed82a789b1ff2fce29121fe65f16ae202d6bbbe8
JJ <jonathanjacobwang@gmail.com>
2023-12-27 19:33:30 -0800

    setup Home with <Rooms />

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+// components
+import Rooms from '../components/Rooms'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Rooms />
+    </>
   )
 }
 
