592d111d102b860a5fe64eef9f4c958a2c3fae5c
JJ <jonathanjacobwang@gmail.com>
2023-12-27 19:15:44 -0800

    set up routes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
+// components
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+
+// pages
+import Home from './pages/Home.jsx';
+import RoomDetails from './pages/RoomDetails.jsx';
+
+// react router
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/room/:id',
+    element: <RoomDetails />,
+  }
+]);
+
 const App = () => {
-  return <div>react app</div>;
+  return <div>
+    <Header />
+    <RouterProvider router={router} />
+    <Footer />
+  </div>;
 };
 
 export default App;
